600048f9a0440eaa8a5cdc70ae9ab271e1a4f189
ChristianMartinezTech <crismartine.e@gmail.com>
2023-04-07 20:21:10 -0500

    Finished the basic templates. Now I need to implement more detailes static files, real images for the products, etc

--- Foodery/food/urls.py
+++ Foodery/food/urls.py
@@ -5,9 +5,12 @@ from django.urls import path
 from . import views
 
 
+app_name = 'food'
+
 # Defining Food App URLs
 urlpatterns = [
     path('', views.index, name="index"),
     path('hello/', views.hello, name="hello"),
-    path('item/', views.item, name='item'),
+    path('restaurant/<int:restaurant_id>/', views.restaurant_detail, name='restaurant_detail'),
+    path('restaurant/<int:restaurant_id>/plate/<int:plate_id>/', views.plate_detail, name='plate_detail'),
 ]
--- Foodery/food/views.py
+++ Foodery/food/views.py
@@ -6,28 +6,56 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Restaurant, Plate
 
-# food index view
+
+# Index View
 def index(request):
     # Getting Restaurants and Plates
     restaurant_list = Restaurant.objects.all()
     plates_list = Plate.objects.all()
     
-    template = loader.get_template('food/index.html') # Getting HTML template
+    #template = loader.get_template('food/index.html') Getting HTML template
 
-    # creating fake contest
+    # creating context
     context = {
         'restaurant_list': restaurant_list,
         'plates_list': plates_list,
     }
     
-    return HttpResponse(template.render(context, request)) # Sending template, context, and request
+    return render(request, 'food/index.html', context) # Remdering request, template, and context
+
 
 # Greeting View
 def hello(request):
-    return HttpResponse("Hi there! Thank you for using Foodery. :)")
+    return HttpResponse("Hi there! Thank you for using Foodery. :)") # Way to not use context
 
-# Item view
-def item(request):
-    return HttpResponse("<h1>this is an item view</h1>")
 
+# Restaurant detail view
+def restaurant_detail(request, restaurant_id):
+
+    restaurant_detail = Restaurant.objects.get(pk=restaurant_id)
+    plates_list = Plate.objects.filter(restaurant=Restaurant.objects.get(pk=restaurant_id))
+
+    # template = loader.get_template('food/restaurant-detail.html') Getting HTML template
+    
+    # creating context
+    context = {
+        'restaurant_detail': restaurant_detail,
+        'plates_list': plates_list,
+    }
+    
+    return render(request, 'food/restaurant-detail.html', context)
+
+
+# Plate detail view
+def plate_detail(request, plate_id, restaurant_id):
+    
+    restaurant_detail = Restaurant.objects.get(pk=restaurant_id)
+    plate_detail = Plate.objects.get(id=plate_id)
+
+    context = {
+        'restaurant_id': restaurant_detail,
+        'plate_detail': plate_detail,
+    }
+
+    return render(request, 'food/plate-detail.html', context)
 
